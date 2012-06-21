af64d3cfc05273ddee87a3ab4a611ba8a1a7eca0
Dan Parks <dan@eastofcleveland.com>
2012-06-20 23:43:03 -0400

    added tile reference file

---
+++ tiles_world1.js
@@ -0,0 +1,18 @@
+{
+  "width":24,
+  "height":8,
+  "tileWidth":44,
+  "tileHeight":33,
+  "animations":[
+    {"name":"dirt1","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":0,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"dirt2","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":44,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"grass","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":88,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"grassTop","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":132,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"field","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":176,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"fieldTop","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":220,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"dirtField","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":264,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"dirtGrass","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":308,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false},
+    {"name":"water","url":"http://localhost/blueBear/res/ground_basic.png","nbframes":1,"rate":0,"offsetx":352,"offsety":0,"delta":0,"nbanimations":null,"distance":null,"type":"ANIMATION_HORIZONTAL","multiple":false}
+  ]
+}
+
