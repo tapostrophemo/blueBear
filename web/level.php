55d6bde29026dcc8754c89e5639d2111f303b3c8
Dan Parks <dan@eastofcleveland.com>
2012-07-12 20:35:49 -0400

    setup level 6; validate level/score params

--- web/level.php
+++ web/level.php
@@ -1,7 +1,11 @@
 <?php
-// TODO: regex/validation that 1) (score|level) is numeric, and 2) levelis an existing level
+
 $level = $_GET['level'];
+if (!preg_match('/^\d+$/', $level)) $level = 1;
+
 $score = $_GET['score'];
+if (!preg_match('/^\d+$/', $score)) $score = 0;
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -18,6 +22,7 @@ $score = $_GET['score'];
  <div id="paused" class="statusOverlay hidden">PAUSED<span>(press ESC to continue)</span></div>
  <div id="win" class="statusOverlay hidden">WINNER!<span>(<a id="nextLevelLink" href="level.php?level=<?=($level+1)?>&score=">click for next level</a>)</span></div>
  <div id="lose" class="statusOverlay hidden">YOU LOST...<br/>SORRY&nbsp;&nbsp;&nbsp;:(<span>(<a href="#" onclick="window.location.reload()">click to try again</a>)</span></div>
+ <div id="underConstruction" class="statusOverlay hidden">level <?=$level?> under construction</div>
 </div>
 <div id="scoreboard">
  <label id="score">Score: <span><?=$score?></span></label><br/>
@@ -36,15 +41,35 @@ $score = $_GET['score'];
 
 <script type="text/javascript" src="res/jquery-1.7.2.min.js"></script>
 <script type="text/javascript" src="res/jquery.gamequery-0.6.2.js"></script>
+
+
+<?php if (!file_exists("levels/${level}.js")): ?>
+
+<script type="text/javascript">
+PLAYGROUND_WIDTH = 352;
+PLAYGROUND_HEIGHT = 264;
+$(function () {
+  $("#playground").playground({width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT});
+  $("#start").addClass("hidden");
+  $("#underConstruction").toggleClass("hidden");
+});
+</script>
+
+<?php else: ?>
+
 <script type="text/javascript" src="res/hammer.js"></script>
 <script type="text/javascript" src="res/jquery.hammer.js"></script>
 <script type="text/javascript" src="res/game_globals.js"></script>
 <script type="text/javascript" src="res/game_objects.js"></script>
 <script type="text/javascript">
-<?php require("levels/".$level.".js"); ?>
+<?php require("levels/${level}.js"); ?>
 score = <?=$score?>;
 </script>
 <script type="text/javascript" src="res/game_main.js"></script>
+
+<?php endif; ?>
+
+
 </body>
 </html>
 
--- web/levels/6.js
+++ web/levels/6.js
@@ -6,78 +6,77 @@ function setupAnimations() {
 
 function setupLayers() {
 // Generated with gQ's Tiles map editor
-var map = [[1, 3, 3, 3, 1, 2, 3, 3, 3, 2, 1, 3, 3, 3, 9, 9, 3, 3, 5, 5, 5, 3, 9, 9],
-           [3, 3, 3, 1, 2, 3, 3, 3, 2, 1, 3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 3, 3, 9, 3],
-           [3, 3, 1, 2, 3, 3, 3, 2, 1, 3, 3, 3, 9, 9, 3, 3, 5, 5, 5, 3, 9, 3, 3, 3],
-           [3, 1, 2, 3, 3, 3, 2, 1, 3, 3, 3, 9, 9, 3, 3, 5, 5, 5, 3, 9, 9, 3, 3, 3],
-           [1, 2, 3, 3, 3, 2, 1, 3, 3, 3, 9, 9, 3, 3, 5, 5, 5, 3, 9, 9, 3, 3, 3, 2],
-           [2, 3, 3, 3, 2, 1, 3, 3, 3, 9, 9, 3, 3, 5, 5, 5, 3, 9, 9, 3, 3, 3, 2, 1],
-           [3, 3, 3, 2, 1, 3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 3, 9, 9, 3, 3, 3, 2, 1, 2],
-           [3, 3, 2, 1, 3, 3, 3, 9, 9, 3, 3, 5, 5, 5, 3, 9, 9, 3, 3, 3, 2, 1, 2, 1]];
+var map = [[5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
+           [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1],
+           [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 2, 1, 2, 1, 2, 1, 2, 1, 5, 5, 2, 1, 2, 1, 2],
+           [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 1, 2, 1, 5, 5, 5, 5, 5, 5, 2, 1, 2, 1],
+           [5, 5, 5, 3, 3, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 1, 2],
+           [5, 5, 3, 3, 9, 9, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 3, 3, 5, 5, 5, 5, 1],
+           [5, 3, 3, 9, 9, 9, 9, 3, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 3, 3, 9, 3, 3, 5, 5, 5, 5],
+           [5, 3, 9, 9, 9, 9, 9, 9, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 9, 9, 9, 9, 3, 5, 5, 5, 5]];
 
   var bushes = [
-    [TILE_WIDTH *  2,   TILE_HEIGHT * 5],
-    [TILE_WIDTH *  4,   TILE_HEIGHT * 3],
-    [TILE_WIDTH *  6,   TILE_HEIGHT * 1],
-    [TILE_WIDTH * 14,   TILE_HEIGHT * 4],
+    [TILE_WIDTH *  0.8, TILE_HEIGHT * 2],
+    [TILE_WIDTH *  1.5, TILE_HEIGHT * 3],
+    [TILE_WIDTH *  5.5, TILE_HEIGHT * 3.5],
+    [TILE_WIDTH *  6.5, TILE_HEIGHT * 2.5],
+    [TILE_WIDTH * 10.5, TILE_HEIGHT * 4],
+    [TILE_WIDTH * 11.5, TILE_HEIGHT * 1.5],
+    [TILE_WIDTH * 13.5, TILE_HEIGHT * 7],
+    [TILE_WIDTH * 13.5, TILE_HEIGHT * 6.7],
+    [TILE_WIDTH * 13.5, TILE_HEIGHT * 5.9],
     [TILE_WIDTH * 14,   TILE_HEIGHT * 5],
-    [TILE_WIDTH * 16,   TILE_HEIGHT * 2],
-    [TILE_WIDTH * 16,   TILE_HEIGHT * 3],
-    [TILE_WIDTH * 17,   TILE_HEIGHT * 0],
-    [TILE_WIDTH * 18,   TILE_HEIGHT * 0],
-    [TILE_WIDTH * 19,   TILE_HEIGHT * 0],
-    [TILE_WIDTH * 19,   TILE_HEIGHT * 7],
-    [TILE_WIDTH * 21.5, TILE_HEIGHT * 5.5],
-    [TILE_WIDTH * 23,   TILE_HEIGHT * 3]];
+    [TILE_WIDTH * 14.5, TILE_HEIGHT * 0],
+    [TILE_WIDTH * 17,   TILE_HEIGHT * 1],
+    [TILE_WIDTH * 17.5, TILE_HEIGHT * 1.5],
+    [TILE_WIDTH * 17,   TILE_HEIGHT * 4.5],
+    [TILE_WIDTH * 17.5, TILE_HEIGHT * 4.5],
+    [TILE_WIDTH * 18,   TILE_HEIGHT * 4.5],
+    [TILE_WIDTH * 20.5, TILE_HEIGHT * 0],
+    [TILE_WIDTH * 23.5, TILE_HEIGHT * 1.5],
+    [TILE_WIDTH * 25.5, TILE_HEIGHT * 2.5],
+    [TILE_WIDTH * 26.5, TILE_HEIGHT * 4.5],
+    [TILE_WIDTH * 26.5, TILE_HEIGHT * 5.5]];
   var borders = [
-    // middle lakes
+    // left lake
+    [ 2, 7],
+    [ 3, 7],
+    [ 3, 6],
+    [ 4, 6],
+    [ 4, 5],
+    [ 5, 5],
+    [ 5, 6],
+    [ 6, 6],
+    [ 6, 7],
     [ 7, 7],
-    [ 8, 7],
-    [ 9, 5],
-    [10, 5],
-    [10, 4],
-    [11, 4],
-    [11, 3],
-    [12, 3],
-    [12, 2],
-    [13, 2],
-    [14, 0],
-    [15, 0],
-    // end lakes
-    [15, 7],
-    [16, 7],
-    [16, 6],
-    [17, 6],
-    [17, 5],
-    [18, 5],
-    [18, 4],
-    [19, 4],
-    [19, 3],
-    [20, 3],
-    [20, 2],
-    [22, 1],
-    [22, 0],
-    [23, 0]];
+    // right lake
+    [19, 7],
+    [20, 7],
+    [21, 7],
+    [21, 6],
+    [22, 7]];
   var honeypots = [
-    [TILE_WIDTH *  1.5, TILE_HEIGHT * 6.5],
-    [TILE_WIDTH *  4.5, TILE_HEIGHT * 5.5],
+    [TILE_WIDTH *  1.5, TILE_HEIGHT * 5.5],
+    [TILE_WIDTH *  4.5, TILE_HEIGHT * 0.5],
+    [TILE_WIDTH *  4.5, TILE_HEIGHT * 1],
     [TILE_WIDTH *  9,   TILE_HEIGHT * 0],
     [TILE_WIDTH * 10,   TILE_HEIGHT * 0],
-    [TILE_WIDTH * 12,   TILE_HEIGHT * 4],
-    [TILE_WIDTH * 13,   TILE_HEIGHT * 4],
-    [TILE_WIDTH * 20,   TILE_HEIGHT * 0.5],
-    [TILE_WIDTH * 20.5, TILE_HEIGHT * 1],
-    [TILE_WIDTH * 21,   TILE_HEIGHT * 1.5],
-    [TILE_WIDTH * 22,   TILE_HEIGHT * 2],
-    [TILE_WIDTH * 17,   TILE_HEIGHT * 7],
-    [TILE_WIDTH * 18,   TILE_HEIGHT * 7],
-    [TILE_WIDTH * 18,   TILE_HEIGHT * 6]];
+    [TILE_WIDTH * 11.5, TILE_HEIGHT * 6],
+    [TILE_WIDTH * 14.5, TILE_HEIGHT * 5.5],
+    [TILE_WIDTH * 14.5, TILE_HEIGHT * 6],
+    [TILE_WIDTH * 17,   TILE_HEIGHT * 2.5],
+    [TILE_WIDTH * 17.5, TILE_HEIGHT * 5.5],
+    [TILE_WIDTH * 18,   TILE_HEIGHT * 0],
+    [TILE_WIDTH * 18.5, TILE_HEIGHT * 0],
+    [TILE_WIDTH * 23.5, TILE_HEIGHT * 3],
+    [TILE_WIDTH * 25.5, TILE_HEIGHT * 4.5],
+    [TILE_WIDTH * 26.5, TILE_HEIGHT * 0.5]];
 
   var i;
 
   var mainGroup = $.playground().addGroup("main", {width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT});
   mainGroup.addGroup("background", {width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
-      .addTilemap('tilemap', map,  animations, {width: 44, height: 33, sizex: 24, sizey: 8}).end().end();
+      .addTilemap('tilemap', map,  animations, {width: 44, height: 33, sizex: 28, sizey: 8}).end().end();
   mainGroup.addSprite("finishLine", {
     animation: animations["finishLine"],
     posx: $("#tilemap").width()-TILE_WIDTH,
@@ -87,7 +86,7 @@ var map = [[1, 3, 3, 3, 1, 2, 3, 3, 3, 2, 1, 3, 3, 3, 9, 9, 3, 3, 5, 5, 5, 3, 9,
   mainGroup.addSprite("player", {
     animation: animations["playerStoppedRight"],
     posx: 0 + TILE_WIDTH / 2,
-    posy: TILE_HEIGHT*2 + SPRITE_HEIGHT/2,
+    posy: TILE_HEIGHT*0 + SPRITE_HEIGHT/2,
     width: SPRITE_WIDTH,
     height: SPRITE_HEIGHT});
 
